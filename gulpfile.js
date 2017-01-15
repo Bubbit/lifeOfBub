6b882dd11feb46b29d63d6f1c8c696c54f75b3ef
Remco <remco090@gmail.com>
2017-01-15 09:21:49 +0100

    remove strict

--- gulpfile.js
+++ gulpfile.js
@@ -8,8 +8,6 @@
  * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
  */
 
-'use strict';
-
 // Documentation on what goes into PolymerProject.
 const path = require('path');
 const gulp = require('gulp');
