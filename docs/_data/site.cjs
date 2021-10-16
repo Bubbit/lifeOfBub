610586c649226b97dca787d67fd6986c66a8b35b
Bubbit <remco090@gmail.com>
2021-10-16 14:40:37 +0200

    chore: change footer and header

---
+++ docs/_data/site.cjs
@@ -0,0 +1,17 @@
+module.exports = async function () {
+    return {
+      dir: 'ltr',
+      lang: 'en',
+      name: 'Bubbit',
+      description: 'Remco Gubbels his personal website',
+      socialLinks: [
+        {
+          name: 'GitHub',
+          url: 'https://github.com/Bubbit',
+        }
+      ],
+      gitSiteUrl: 'https://github.com/Bubbit/lifeOfBub',
+      gitBranch: 'master',
+      helpUrl: 'https://github.com/Bubbit/lifeOfBub/issues'
+    };
+  };
